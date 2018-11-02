a37dae1e49219130a03ae9fc51068c5055bc7061
Peter <peter.niemeier@gmail.com>
2018-11-02 14:49:03 -0700

    Add start script

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
 
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'index.ejs'),
-            filename: './index.ejs',
+            filename: 'index.ejs',
             title: appHtmlTitle
         })
     ],
